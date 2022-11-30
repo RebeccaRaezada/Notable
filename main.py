c5662fed8a669212e63467f4a43f6ab5582090f4
Rebecca Raezada <rebecca.raezada22@gmail.com>
2022-11-29 16:40:23 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 def create_numbered_list(transcribed_text):
+    
+    # making input case consistent
     lowercase_transcribed_text = transcribed_text.lower()
+    
+    # initializing variables
     index_mapping = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
-    result = {}
+    numbered_list_extract = {}
     counter = 1
+    
+    # capturing introductory text, before the numbered list
     pre_numbered_list_extract = lowercase_transcribed_text.split("number")[0][0].upper() + lowercase_transcribed_text.split("number")[0][1:]
 
-    text_splits = lowercase_transcribed_text.split("number")[1]
-    next_text_splits = lowercase_transcribed_text.split("number next")[1:]
-    location = index_mapping[text_splits.split(" ")[1]]
-    first_line = ' '.join(text_splits.split(" ")[2:])
-    result[location] = (first_line[0].upper() + first_line[1:])
-    for sentence_parts in next_text_splits:
-        phrase = (' '.join(sentence_parts.split(" ")[2:]))
-        location += 1
-        result[location] = (phrase[0].upper() + phrase[1:])
+    # capturing phrases and starting location for numbered list of phrases
+    first_phrase = lowercase_transcribed_text.split("number")[1]
+    next_phrases = lowercase_transcribed_text.split("number next")[1:]
+    
+    start_location = index_mapping[first_phrase.split(" ")[1]]
+    first_line = ' '.join(first_phrase.split(" ")[2:])
+    
+    # getting numbered list
+    numbered_list_extract[start_location] = (first_line[0].upper() + first_line[1:])
+    for phrases in next_phrases:
+        phrase = (' '.join(phrases.split(" ")[2:]))
+        start_location += 1
+        numbered_list_extract[start_location] = (phrase[0].upper() + phrase[1:])
         counter += 1
 
-    return result, pre_numbered_list_extract
+    return pre_numbered_list_extract, numbered_list_extract
 
 
-transcription = "Patient presents today with several issues. " \
+test_input = "Patient presents today with several issues. " \
                 "Number one BMI has increased by 10% since their " \
                 "last visit number next patient reports experiencing " \
                 "dizziness several times in the last two weeks. Number " \
@@ -27,7 +37,7 @@ transcription = "Patient presents today with several issues. " \
                 "for last 4 weeks Number next patient is taking drug number " \
                 "five several times a week."
 
-result, pre_numbered_list_extract = create_numbered_list(transcription)
+pre_numbered_list_extract, result = create_numbered_list(test_input)
 print(pre_numbered_list_extract)
 for each_line in result:
     print(each_line, result[each_line])
